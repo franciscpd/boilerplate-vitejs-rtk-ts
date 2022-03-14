8deb627047997b4900c62713258cc1a8c8b0816f
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-03-14 08:04:20 -0300

    feat: initial setup with vitejs and redux toolkit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,52 @@
+import { increment } from '../../features/counter/counter-slice'
+import { useAppDispatch, useAppSelector } from '../../hooks'
+
+import logo from '../../logo.svg'
+
+import './styles.css'
+
+function App() {
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  const handleClick = () => {
+    dispatch(increment())
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <button type="button" onClick={handleClick}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
