0acd8248928311e0a7f32da577bf6d8d2779dcd1
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-07-10 22:38:14 -0300

    feat: disable devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit'
 
-import counterReducer from '../features/counter/counter-slice';
+import counterReducer from '../features/counter/counter-slice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware();
+    return getDefaultMiddleware()
   },
-});
+  devTools: !import.meta.env.PROD,
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
